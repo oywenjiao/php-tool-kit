e84e0db30aebbf4a3220f1716447835a0a4fbb6d
ouyang <wenjiao8605@126.com>
2021-05-10 09:48:31 +0800

    add

---
+++ src/Wechat/WxHandle.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace Wj\ToolKit\Wechat;
+
+
+class WxHandle
+{
+
+    /**
+     * 将参数格式化成url拼接参数
+     * @param $arr
+     * @return string
+     */
+    public function ToUrlParams($arr)
+    {
+        $buff = "";
+        foreach ($arr as $k => $v) {
+            if ($k != "sign" && $v != "" && !is_array($v)) {
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+
+    /**
+     * 格式化后输出xml字符
+     * @param $arr
+     * @return bool|string
+     */
+    public function ToXml($arr)
+    {
+        if (!is_array($arr) || count($arr) <= 0) {
+            return false;
+        }
+
+        $xml = "<xml>";
+        foreach ($arr as $key => $val) {
+            if (is_numeric($val)) {
+                $xml .= "<" . $key . ">" . $val . "</" . $key . ">";
+            } else {
+                $xml .= "<" . $key . "><![CDATA[" . $val . "]]></" . $key . ">";
+            }
+        }
+        $xml .= "</xml>";
+        return $xml;
+    }
+
+    /**
+     * 将xml转为array
+     * @param string $xml
+     * @return array|bool
+     */
+    public function fromXml($xml)
+    {
+        if (!$xml) {
+            return false;
+        }
+        //将XML转为array
+        //禁止引用外部xml实体
+        libxml_disable_entity_loader(true);
+        return json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+    }
+}
